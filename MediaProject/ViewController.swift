e7174ee98df11c354729f662bc24b86434ad8c45
Damien-B <bannerotdamien@gmail.com>
2016-11-09 12:27:00 +0100

    initial commit

---
+++ MediaProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaProject
+//
+//  Created by Damien Bannerot on 09/11/2016.
+//  Copyright © 2016 Damien Bannerot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
